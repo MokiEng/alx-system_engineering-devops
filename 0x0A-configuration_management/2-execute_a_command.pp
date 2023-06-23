a709e49adab5f40fc96eff31b229e5e3696b8fdc
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-06-23 06:54:33 -0700

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,6 +2,7 @@
 
 exec { 'killmenow':
   command     => 'pkill killmenow',
+  provider    => 'shell',
   onlyif      => 'pgrep killmenow', # Ensure the process is running before killing
   refreshonly => true, # Only run the command when explicitly refreshed
 }
