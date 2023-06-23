e3b488d9f8fd2f9a01342529f40946dda575abb2
MokiEng <meconen20@gmail.com>
2023-06-23 06:47:57 -0700

    Execute a command using a puppet

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# Using Puppet, create a manifest that kills a process named killmenow.
+
+exec { 'killmenow':
+  command     => 'pkill killmenow',
+  path        => '/usr/bin:/bin',
+  onlyif      => 'pgrep killmenow', # Ensure the process is running before killing
+  refreshonly => true, # Only run the command when explicitly refreshed
+}
